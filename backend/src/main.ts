00e370ed518fb6acc510d76c576fb70e7335cf81
pa.nenashev <pa.nenashev@vkteam.ru>
2026-07-03 13:13:17 +0700

    YP: adds cors setup
    YP: adds cors setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,20 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.useGlobalGuards(new JwtGuard(reflector));
+
+  app.enableCors({
+    origin: [
+      'https://pavnen.diploma.nomorepartiessite.ru',
+      'http://pavnen.diploma.nomorepartiessite.ru',
+      'http://localhost:4000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(3000);
 }
 
